72873936d3b89d6fc1e28c09caf84e812430407b
PLMCHL <pm.deblois@gmail.com>
2024-12-02 20:57:05 -0500

    Add color to rating based on bracket ratings

--- pickleballbrackets.user.js
+++ pickleballbrackets.user.js
@@ -39,10 +39,21 @@ function handleMutation(mutation) {
 }
 
 function handlePlayerRow(team_container) {
+    // Get bracket low/high
+    const [_0, _1, _2, _3, _4, _5] = $(team_container)
+        .closest(".table-players")
+        .prev()
+        .find("h4")
+        .text()
+        .trim()
+        .match(/^.*(.*) Skill: \((.*) (To|And) (.*)\) Age: .*\).*$/);
+    const bracket_low = parseFloat(_2);
+    const bracket_high = parseFloat(_4);
+
+    // Fetch DUPRs
     const player_name_containers = $(team_container)
         .find(".removetag")
         .toArray();
-
     for (const player_name_container of player_name_containers) {
         const player_name_a = $(player_name_container).find("a").get(0);
         const player_name = $(player_name_a).text();
@@ -51,13 +62,18 @@ function handlePlayerRow(team_container) {
             method: "POST",
             url: DUPR_SEARCH_URL,
             data: JSON.stringify({
-                filter: {},
-                includeUnclaimedPlayers: true,
-                limit: 10,
-                offset: 0,
+                filter: {
+                    rating: {
+                        category: "DUPR",
+                        maxRating: 10,
+                        minRating: 0,
+                        // TODO: type: "DOUBLES",
+                    },
+                },
+                includeUnclaimedPlayers: false,
+                limit: 2,
                 pageSource: "LD_ADD_PARTICIPANT",
                 query: player_name,
-                verifiedEmail: true,
             }),
             headers: {
                 "Content-Type": "application/json",
@@ -73,6 +89,7 @@ function handlePlayerRow(team_container) {
                 if (hits.length < 1) {
                     rating = "NF";
                 } else if (hits.length > 1) {
+                    // TODO: Add link to player search
                     console.log("Too many hits for player");
                 } else {
                     rating = hits[0].ratings.doubles;
@@ -80,7 +97,13 @@ function handlePlayerRow(team_container) {
 
                 $(team_container)
                     .find("td:nth-child(1)")
-                    .after(getTableItem(rating));
+                    .after(
+                        getTableItem(rating).css(
+                            "color",
+                            getRatingColor(rating, bracket_low, bracket_high)
+                        )
+                    )
+                    .remove();
             },
         });
     }
@@ -90,4 +113,19 @@ function getTableItem(value) {
     return $(
         `<td style="min-width: 40px; max-width: 40px; width: 40px; text-align:center; " class="p-t-5 p-b-5">${value}</td>`
     );
+}
+
+function getRatingColor(rating, low, high) {
+    if (rating < low) {
+        return "blue";
+    }
+
+    if (rating > high) {
+        if (rating - high < 0.5) {
+            return "orange";
+        }
+        return "red";
+    }
+
+    return undefined;
 }
